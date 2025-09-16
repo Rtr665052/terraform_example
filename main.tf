12fd038c963bb874a57b0c56dca259b23e1bb310
repathomasv4a <thomas.c.repa.mil@army.mil>
2025-09-16 16:13:52 -0400

    fix image

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "main_subnet" {
 }
 
 resource "aws_instance" "my_app" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-2 (example)
+  ami           = "ami-0fe972392d04329e1" # Amazon Linux 2 in us-east-2 (example)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main_subnet.id
 

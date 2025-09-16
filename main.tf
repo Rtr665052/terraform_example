8ca6b04815c467a754a87a1e9e9cdb6f1a511815
repathomasv4a <thomas.c.repa.mil@army.mil>
2025-09-16 17:09:20 -0400

    add ip launch

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.pipeline_vpc.id
   cidr_block        = "192.168.1.0/24"
   availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "new_subnet"
   }

8177b7c1cdd470f7c49341bc15e99523038be0d9
repathomasv4a <thomas.c.repa.mil@army.mil>
2025-09-16 16:41:43 -0400

    add ig

--- main.tf
+++ main.tf
@@ -29,6 +29,24 @@ resource "aws_subnet" "main_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.pipeline_vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.pipeline_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
 resource "aws_instance" "my_app" {
   ami           = "ami-0fe972392d04329e1" # Amazon Linux 2 in us-east-2 (example)
   instance_type = "t2.micro"
